cc9615b93c46e06bdcce7c45131a850a2045726c
k1jt <k1jt@b3ca0830-8508-0410-8253-dac51d8a7955>
2008-03-18 12:13:45 +0000

    Added chkt0.f90 (should have done so before ...)
    git-svn-id: svn://svn.berlios.de/wsjt/trunk@639 b3ca0830-8508-0410-8253-dac51d8a7955

---
+++ chkt0.f90
@@ -0,0 +1,28 @@
+subroutine chkt0(nplain,naddon,ndiff)
+
+!f2py intent(out) nplain,naddon,ndiff
+  integer dgen(12)
+  character*22 t0msg2
+  include 'gcom2.f90'
+
+  call packmsg(t0msg,dgen)
+  call unpackmsg(dgen,t0msg2)
+  if(index(t0msg,'/').gt.0) then
+     naddon=1
+  else
+     naddon=0
+  endif
+  if(iand(dgen(10),8).ne.0) then
+     nplain=1
+     naddon=0
+  else
+     nplain=0
+  endif
+  if(t0msg2.ne.t0msg) then
+     ndiff=1
+  else
+     ndiff=0
+  endif
+
+  return
+end subroutine chkt0
