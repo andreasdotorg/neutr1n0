dfe5e39d5547ac27c20908c9a223975f0dd66c03
k1jt <k1jt@b3ca0830-8508-0410-8253-dac51d8a7955>
2009-01-05 15:26:47 +0000

    Changed version name to 7.03.
    git-svn-id: svn://svn.berlios.de/wsjt/trunk@1048 b3ca0830-8508-0410-8253-dac51d8a7955

--- wsjt.py
+++ wsjt.py
@@ -24,7 +24,7 @@ from types import *
 import array
 
 root = Tk()
-Version="7.02 r" + "$Rev$"[6:-1]
+Version="7.03 r" + "$Rev$"[6:-1]
 print "******************************************************************"
 print "WSJT Version " + Version + ", by K1JT"
 print "Revision date: " + \
