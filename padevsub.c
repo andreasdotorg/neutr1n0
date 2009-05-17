7ebb76e7688bfdd0211cce20a2debd80b5cfb633
va3db <va3db@b3ca0830-8508-0410-8253-dac51d8a7955>
2009-05-17 01:30:40 +0000

    - Use <portaudio.h> not "portaudio.h"
    git-svn-id: svn://svn.berlios.de/wsjt/trunk@1232 b3ca0830-8508-0410-8253-dac51d8a7955

--- padevsub.c
+++ padevsub.c
@@ -1,6 +1,6 @@
 // #include <stdio.h>
 // #include <math.h>
-#include "portaudio.h"
+#include <portaudio.h>
 
 int __stdcall PADEVSUB(int *numdev, int *ndefin, int *ndefout,
 		       int nchin[], int nchout[])
