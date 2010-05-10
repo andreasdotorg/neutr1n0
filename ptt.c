7599635a3c40de063b93d99b90ad9c4c0d4a152f
k1jt <k1jt@b3ca0830-8508-0410-8253-dac51d8a7955>
2010-05-10 16:11:19 +0000

    Add 100 to COM port number to hold it open after first use.
    git-svn-id: svn://svn.berlios.de/wsjt/trunk@1912 b3ca0830-8508-0410-8253-dac51d8a7955

--- ptt.c
+++ ptt.c
@@ -4,7 +4,7 @@
 int ptt_(int *nport, char *unused, int *ntx, int *iptt)
 {
   static HANDLE hFile;
-  static int open=0;
+  static int open=0, nhold=0;
   char s[10];
   int i3,i4,i5,i6,i9,i00;
 
@@ -13,8 +13,11 @@ int ptt_(int *nport, char *unused, int *ntx, int *iptt)
     return(0);
   }
 
+  nhold=0;
+  if(*nport>100) nhold=1;
+
   if(*ntx && (!open)) {
-    sprintf(s,"\\\\.\\COM%d",*nport);
+    sprintf(s,"\\\\.\\COM%d",*nport%100);
     hFile=CreateFile(
 		     TEXT(s),
 		     GENERIC_WRITE,
@@ -25,7 +28,7 @@ int ptt_(int *nport, char *unused, int *ntx, int *iptt)
 		     NULL
 		     );
     if(hFile==INVALID_HANDLE_VALUE) {
-      printf("PTT: Cannot open COM port %d.\n",*nport);
+      printf("PTT: Cannot open COM port %d.\n",*nport%100);
       return(1);
     }
     open=1;
@@ -41,9 +44,11 @@ int ptt_(int *nport, char *unused, int *ntx, int *iptt)
     EscapeCommFunction(hFile,4);
     EscapeCommFunction(hFile,6);
     EscapeCommFunction(hFile,9);
-    i00=CloseHandle(hFile);
+    if(nhold==0)  {
+      i00=CloseHandle(hFile);
+      open=0;
+    }
     *iptt=0;
-    open=0;
   }
   return(0);
 }
